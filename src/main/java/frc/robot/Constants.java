48e9ad0340223ec241cb55233c94618fa11a8091
q-field <q@quentinfield.com>
2026-02-01 14:38:11 -0500

    update canids to use less of the number line

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -221,11 +221,11 @@ public final class Constants {
             "frontright",
             new Coordinates(-TRACK_WIDTH_METRES / 2, WHEEL_BASE_METRES / 2),
             SDS_MK4I_WHEEL_RADIUS_M,
-            20,
+            15,
             DRIVE_MOTOR_CONFIG,
-            21,
+            16,
             ANGLE_MOTOR_CONFIG,
-            22,
+            17,
             Rotation2d.fromRotations(0.411377).getDegrees(),
             ANGLE_ENCODER_CONFIG);
 
@@ -234,11 +234,11 @@ public final class Constants {
             "backright",
             new Coordinates(TRACK_WIDTH_METRES / 2, WHEEL_BASE_METRES / 2),
             SDS_MK4I_WHEEL_RADIUS_M,
-            30,
+            20,
             DRIVE_MOTOR_CONFIG,
-            31,
+            21,
             ANGLE_MOTOR_CONFIG,
-            32,
+            22,
             Rotation2d.fromRotations(0.353271).getDegrees(),
             ANGLE_ENCODER_CONFIG);
 
@@ -247,11 +247,11 @@ public final class Constants {
             "backleft",
             new Coordinates(TRACK_WIDTH_METRES / 2, -WHEEL_BASE_METRES / 2),
             SDS_MK4I_WHEEL_RADIUS_M,
-            40,
+            25,
             DRIVE_MOTOR_CONFIG,
-            41,
+            26,
             ANGLE_MOTOR_CONFIG,
-            42,
+            27,
             Rotation2d.fromRotations(0.506836).getDegrees(),
             ANGLE_ENCODER_CONFIG);
 
