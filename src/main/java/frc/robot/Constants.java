8982338e1e41501cd707115af0d4bb5a53fcf842
Jeff Zakrzewski <jeff@911incident.com>
2026-02-17 18:02:02 -0500

    New treads

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -152,7 +152,7 @@ public final class Constants {
     /** Side to side from the middle of the wheels */
     public static final double TRACK_WIDTH_METRES = inchesToMeters(16.75);
 
-    public static final double SDS_MK4I_WHEEL_RADIUS_M = 0.0490;
+    public static final double SDS_MK4I_WHEEL_RADIUS_M = 0.0510;
 
     public static final GyroConfig GYRO_CONFIG = GyroConfig.pigeon2(8, true);
 
